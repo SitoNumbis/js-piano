f5433403a9c9844f4618c7118dd1e6b96ea6747b
SCiytcor <carlos.morag@estudiantes.uo.edu.cu>
2021-09-24 21:14:20 -0400

    remove active css attribute done

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 const whites = ['a', 's', 'd', 'f', 'g', 'h', 'j'];
 const blacks = ['w', 'e', 't', 'y', 'u'];
+const allKeys = ['a', 'w', 's', 'e', 'd', 'f', 't', 'g', 'y', 'h', 'u', 'j'];
 
 const notes = ['C', 'Cs', 'D', 'Ds', 'E', 'F', 'Fs', 'G', 'Gs', 'A', 'As', 'B'];
 
@@ -13,9 +14,11 @@ notes.forEach((key) => {
     else
         div.classList.add('black');
 
-
     div.id = key;
     div.dataset.note = key;
+
+    let h1 = document.createElement('h1');
+    h1.innerText = 
 
     document.getElementsByClassName('piano')[0].appendChild(div)
 
@@ -30,24 +33,32 @@ const sharps = document.querySelectorAll('.key.black');
 
 keys.forEach((key) => {
     key.addEventListener('click', () => playNote(key));
+    
 });
+
+document.addEventListener('keyup', (e) => {
+    const key = e.key;
+    const keyIndex = allKeys.indexOf(key); 
+    if (keyIndex > -1) keys[keyIndex].classList.remove('active');
+})
 
 let playNote = (key) => {
     const noteSound = document.getElementById(key.dataset.note);
     noteSound.currentTime = 0;
     noteSound.play();
     key.classList.add('active');
-    noteSound.addEventListener('ended', () => {
+    /*noteSound.addEventListener('ended', () => {
       key.classList.remove('active');
-    });
+    });*/
 };
 
 document.addEventListener('keydown', (e) => {
     if (e.repeat) return;
     const key = e.key;
-    const whiteKeyIndex = whites.indexOf(key);
-    const blackKeyIndex = blacks.indexOf(key);
+    const keyIndex = allKeys.indexOf(key);
+    if(keyIndex > -1) playNote(regulars[keyIndex])
+    //const blackKeyIndex = blacks.indexOf(key);
   
-    if (whiteKeyIndex > -1) playNote(regulars[whiteKeyIndex]);
-    if (blackKeyIndex > -1) playNote(sharps[blackKeyIndex]);
+    /*if (whiteKeyIndex > -1) playNote(regulars[whiteKeyIndex]);
+    if (blackKeyIndex > -1) playNote(sharps[blackKeyIndex]);*/
 });
