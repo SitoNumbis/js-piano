b193edd4e8deadbba7cc7d28f65adae969680805
SCiytcor <carlos.morag@estudiantes.uo.edu.cu>
2021-09-25 08:04:42 -0400

    scroll up

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 /*const whites = ['a', 's', 'd', 'f', 'g', 'h', 'j'];
 const blacks = ['w', 'e', 't', 'y', 'u'];*/
-const allKeys = ['q', '2', 'w', '3', 'e', 'r', '5', 't', '6', 'y', '7', 'u',
+const allKeys = ['z', 's', 'x', 'd', 'c', 'v', 'g', 'b', 'h', 'n', 'j', 'm' ,
+                 'q', '2', 'w', '3', 'e', 'r', '5', 't', '6', 'y', '7', 'u',
                  'i', '9', 'o', '0', 'p', '[', '=', ']', 'Backspace', '.', ';', '/'];
 
-const notes = ['C5', 'Cs5', 'D5', 'Ds5', 'E5', 'F5', 'Fs5', 'G5', 'Gs5', 'A5', 'As5', 'B5', 
+const notes = ['C4', 'Cs4', 'D4', 'Ds4', 'E4', 'F4', 'Fs4', 'G4', 'Gs4', 'A4', 'As4', 'B4',
+               'C5', 'Cs5', 'D5', 'Ds5', 'E5', 'F5', 'Fs5', 'G5', 'Gs5', 'A5', 'As5', 'B5', 
                'C6', 'Cs6', 'D6', 'Ds6', 'E6', 'F6', 'Fs6', 'G6', 'Gs6', 'A6', 'As6', 'B6'];
 let keyPressed = [];
 
